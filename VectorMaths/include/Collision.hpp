e9f66be6d3209c820d6f488579b7e500f2c72325
machin0r <machin0r@proton.me>
2025-12-14 15:49:00 +0000

    Add AABB class and methods

--- VectorMaths/include/Collision.hpp
+++ VectorMaths/include/Collision.hpp
@@ -14,3 +14,22 @@ public:
 	// Operations
 	Vec3 getPoint(float t) const;
 };
+
+class AABB {
+public:
+	Vec3 min, max;
+
+	// Constructors
+	AABB();
+	AABB(const Vec3& min, const Vec3& max);
+	static AABB fromCenterAndExtents(const Vec3& center, const Vec3& halfExtents);
+
+
+	// Operations
+	Vec3 getExtents() const;
+	Vec3 getCenter() const;
+	bool contains(const Vec3& point) const;
+	void expand(const Vec3& point);
+	AABB merge(const AABB& other);
+
+};
--- VectorMaths/src/Collision.cpp
+++ VectorMaths/src/Collision.cpp
@@ -11,3 +11,51 @@ Ray::Ray(const Vec3& origin, const Vec3& direction)
 Vec3 Ray::getPoint(float t) const {
 	return origin + (direction * t);
 }
+
+
+AABB::AABB() : min(0.0f, 0.0f, 0.0f), max(0.0f, 0.0f, 0.0f) {}
+AABB::AABB(const Vec3& min, const Vec3& max) : min(min), max(max) {}
+AABB AABB::fromCenterAndExtents(const Vec3& center, const Vec3& halfExtents) {
+	return AABB(center - halfExtents, center + halfExtents);
+}
+
+Vec3 AABB::getExtents() const {
+	return (max - min) / 2;
+}
+
+Vec3 AABB::getCenter() const {
+	return(min + max) / 2;
+}
+
+
+bool AABB::contains(const Vec3& point) const {
+	bool inX = (max.x >= point.x) && (point.x >= min.x);
+	bool inY = (max.y >= point.y) && (point.y >= min.y);
+	bool inZ = (max.z >= point.z) && (point.z >= min.z);
+
+	return inX && inY && inZ;
+}
+
+void AABB::expand(const Vec3& point) {
+	max.x = std::fmax(max.x, point.x);
+	max.y = std::fmax(max.y, point.y);
+	max.z = std::fmax(max.z, point.z);
+	min.x = std::fmin(min.x, point.x);
+	min.y = std::fmin(min.y, point.y);
+	min.z = std::fmin(min.z, point.z);
+}
+
+AABB AABB::merge(const AABB& other) {
+	float maxX = std::fmax(max.x, other.max.x);
+	float maxY = std::fmax(max.y, other.max.y);
+	float maxZ = std::fmax(max.z, other.max.z);
+	float minX = std::fmin(min.x, other.min.x);
+	float minY = std::fmin(min.y, other.min.y);
+	float minZ = std::fmin(min.z, other.min.z);
+
+	Vec3 maxVec(maxX, maxY, maxZ);
+	Vec3 minVec(minX, minY, minZ);
+
+	return AABB(minVec, maxVec);
+}
+
