4d5de5cf27484697487f166d1c45db960da76c94
machin0r <machin0r@proton.me>
2025-12-16 10:44:57 +0000

    Add AABB-AABB intersection function

--- VectorMaths/src/Collision.cpp
+++ VectorMaths/src/Collision.cpp
@@ -143,3 +143,9 @@ bool rayIntersectsAABB(const Ray& ray, const AABB& box, float& distance) {
 
 	return true;
 }
+
+bool aabbIntersectsAABB(const AABB& a, const AABB& b) {
+	return (a.min.x <= b.max.x && a.max.x >= b.min.x) &&
+		(a.min.y <= b.max.y && a.max.y >= b.min.y) &&
+		(a.min.z <= b.max.z && a.max.z >= b.min.z);
+}
